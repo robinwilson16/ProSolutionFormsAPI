d8f336cb6709f98f5b853d66e3771068bfae9426
Robin Wilson <robin.wilson@shcg.ac.uk>
2024-12-03 21:07:36 +0000

    Minor Edits

--- ProSolutionFormsAPI/Models/StudentModel.cs
+++ ProSolutionFormsAPI/Models/StudentModel.cs
@@ -11,6 +11,19 @@ namespace ProSolutionFormsAPI.Models
         public string? Surname { get; set; }
         public string? Forename { get; set; }
         public DateTime? DateOfBirth { get; set; }
+        public string? Email { get; set; }
         public Guid? StudentGUID { get; set; }
+        public string? AcademicYearID { get; set; }
+        public string? TeamCode { get; set; }
+        public string? TeamName { get; set; }
+        public string? CourseCode { get; set; }
+        public string? CourseTitle { get; set; }
+        public string? CompletionStatusCode { get; set; }
+        public string? CompletionStatusName { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? ExpEndDate { get; set; }
+        public DateTime? ActEndDate { get; set; }
+        public int? NumConvictions { get; set; }
+
     }
 }
